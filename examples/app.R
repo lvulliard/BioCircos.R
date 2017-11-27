50038fc8ee77017979468050bfbe4d2a505955af
Loan Vulliard <lvulliard@cemm.oeaw.ac.at>
2017-11-27 09:30:59 +0100

    Add minimal shiny integration example

---
+++ examples/app.R
@@ -0,0 +1,18 @@
+setwd("../")
+devtools::install() # Build library
+library(BioCircos)
+library(shiny)
+
+shinyUi <- fluidPage(
+	mainPanel(BioCircosOutput("testBioCircos")),
+	sidebarPanel(textInput("testInput", "Your input:")),	
+	title = "BioCircos example"
+)
+
+shinyServer <- function(input, output) {
+	output$testBioCircos <- renderBioCircos({
+		BioCircos(input$testInput)
+	})
+}
+
+shinyApp(shinyUi, shinyServer)
